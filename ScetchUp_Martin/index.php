0d9e7907e3e50b6ae2bc0242ccea976de3a4a065
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2020-12-04 11:58:31 +0100

    Uploaded ScetchUp for BurgerCity

---
+++ ScetchUp_Martin/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+</head>
+
+<?php
+    include 'pages/foothead/header.php'
+
+?>
+
+
+
+
+<body>
+    <div class="main-picture">
+
+    </div>
+    <div class="werbe-kacheln">
+        <h3>saftige</h3>
+        <h1>Burger</h1>
+    </div>
+    <div class="werbe-kacheln">
+        <h3 stylle="border">knusprige</h3>
+        <h1>Pommes</h1>
+    </div>
+    <div class="werbe-kacheln">
+        <h3>kühle</h3>
+        <h1>Getränke</h1>
+    </div>
+    </div>
+    <div class="werbe-kacheln">
+        <h3>saftige</h3>
+        <h1>Burger</h1>
+    </div>
+    <div class="werbe-kacheln">
+        <h3 stylle="border">knusprige</h3>
+        <h1>Pommes</h1>
+    </div>
+    <div class="werbe-kacheln">
+        <h3>kühle</h3>
+        <h1>Getränke</h1>
+    </div>
+    </div>
+    <div class="werbe-kacheln">
+        <h3>saftige</h3>
+        <h1>Burger</h1>
+    </div>
+    <div class="werbe-kacheln">
+        <h3 stylle="border">knusprige</h3>
+        <h1>Pommes</h1>
+    </div>
+    <div class="werbe-kacheln">
+        <h3>kühle</h3>
+        <h1>Getränke</h1>
+    </div>
+</body>
+
+
+<footer>
+    <div>
+        Made with ☕ by Daniel, Felix und Martin.
+    </div>
+</footer>
+
+
+
+</html>
