1e13390b58452b4df95f315adfdafe526f968525
DanielMichel350 <73823199+DanielMichel350@users.noreply.github.com>
2021-01-08 15:41:14 +0100

    added config folder

---
+++ Project/config/database.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @author Kristof Friess <kristof.friess@fh-erfurt.de>
+ * @copyright Since 2018 by Kristof Friess
+ * @version 1.0.0
+ */
+
+
+$dbName = 'BurgerCity';
+
+$dns   = 'mysql:dbname='.$dbName.';host=localhost';
+$user  = 'root';
+$pw    = '';
+$options    = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+$db = null;
+
+try
+{
+    $db = new PDO($dns, $user, $pw, $options);
+} 
+catch (PDOException $e)
+{
+    die( 'Database connection failed: ' . $e->getMessage() );
+}
