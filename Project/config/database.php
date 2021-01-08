717db8897914ca348a83a9839050ef4ee9e35b0b
DanielMichel350 <73823199+DanielMichel350@users.noreply.github.com>
2021-01-08 16:08:13 +0100

    Protokoll

--- Project/config/database.php
+++ Project/config/database.php
@@ -1,17 +1,10 @@
 <?php
 
-/**
- * @author Kristof Friess <kristof.friess@fh-erfurt.de>
- * @copyright Since 2018 by Kristof Friess
- * @version 1.0.0
- */
-
-
 $dbName = 'BurgerCity';
 
 $dns   = 'mysql:dbname='.$dbName.';host=localhost';
 $user  = 'root';
-$pw    = '';
+$pw    = 'root';
 $options    = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
