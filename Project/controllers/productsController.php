a47debaa0ff5d873ab6f2fd8c6cae2d88fbab960
DanielMichel350 <73823199+DanielMichel350@users.noreply.github.com>
2021-01-27 16:55:31 +0100

    productsContoller angelegt, pagesController angepa

---
+++ Project/controllers/productsController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace dwp\controller;
+
+
+class ProductsController extends \dwp\core\Controller
+{
+	public function actionCategory()
+	{
+		
+	}
+
+	public function actionMenue()
+	{
+		
+	}
+
+	public function actionProduct()
+	{
+		
+	}
+
+}
