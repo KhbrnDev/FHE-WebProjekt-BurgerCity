565d53d9e9755d64760150475da06c50b83c456a
DanielMichel350 <73823199+DanielMichel350@users.noreply.github.com>
2021-02-21 10:58:25 +0100

    error404

--- Project/views/errors/error404.php
+++ Project/views/errors/error404.php
@@ -10,7 +10,6 @@
                
                 <a  href="index.php?c=pages&a=start"> <img class="link-picture" src="<?=IMAGESPATH.'BBQ_Burger.png'?>" alt=""> </a>
             </div>
-            <p>Das ist ein Text</p>
         </div>
 
     
