f3c593cd7e1faff587db3c6935d239395ec869c1
KhbrnDev <61619428+KhbrnDev@users.noreply.github.com>
2021-02-22 22:28:13 +0100

    Added Button activation only if input is valid

--- Project/assets/js/signin.js
+++ Project/assets/js/signin.js
@@ -6,14 +6,34 @@ var phonenumberRegEx = /^[0-9]{6,}/;
 var emailRegEx = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
 var passwordRegEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
-
+// get Page input
 var firstName = document.getElementById('firstname');
+var lastName = document.getElementById('lastname');
+var birthday = document.getElementById('birthday');
+var phonenumber = document.getElementById('phonenumber');
+var email = document.getElementById('email');
+var password = document.getElementById('password');
+
+// handle Submit button
+var submit = document.getElementById('signin');
+function activateSumbit() 
+{
+    if(firstName.value.length == 0 || lastName.value.length == 0 || birthday.value.length == 0 || phonenumber.value.length == 0 || email.value.length == 0 || password.value.length == 0)
+    {
+        submit.disabled = true;
+    }
+}
+activateSumbit();
+
+// handle validation
 firstName.addEventListener('focusout', function() {
     errorOutput = document.getElementById('wrongFirstName');
     if (firstName.value.match(firstNameRegEx) && firstName.value.length >= 2) 
     {
         firstName.style.border = "1px solid green";
         errorOutput.style.display = "none";
+        submit.disabled = false;
+        activateSumbit() 
         return;
     }
     else
@@ -22,18 +42,19 @@ firstName.addEventListener('focusout', function() {
         errorOutput.style.display = "block";
         errorOutput.style.color = "red";
         errorOutput.innerHTML = "Vorname muss mindestens 2 Buchstaben und<br>keine Zahlen enthalten";
-        
+        submit.disabled = true;
         return;
     }
 });
 
-var lastName = document.getElementById('lastname');
 lastName.addEventListener('focusout', function() {
     errorOutput = document.getElementById('wrongLastName');
     if (lastName.value.match(lastNameRegEx) && lastName.value.length >= 2) 
     {
         lastName.style.border = "1px solid green";
         errorOutput.style.display = "none";
+        submit.disabled = false;
+        activateSumbit() 
         return;
     }
     else
@@ -42,18 +63,20 @@ lastName.addEventListener('focusout', function() {
         errorOutput.style.display = "block";
         errorOutput.style.color = "red";
         errorOutput.innerHTML = "Nachname muss mindestens 2 Buchstaben und<br> keine Zahlen enthalten";
+        submit.disabled = true;
         return;
     }
 });
 
 
-var birthday = document.getElementById('birthday');
 birthday.addEventListener('focusout', function() {
     errorOutput = document.getElementById('wrongBirthday');
     if (birthday.value.match(birthdayRegEx)) 
     {
         birthday.style.border = "1px solid green";
         errorOutput.style.display = "none";
+        submit.disabled = false;
+        activateSumbit() 
         return;
     }
     else
@@ -62,18 +85,19 @@ birthday.addEventListener('focusout', function() {
         errorOutput.style.display = "block";
         errorOutput.style.color = "red";
         errorOutput.innerHTML = "Datum muss im Format tt.mm.jjjj und<br> ein valides Geburtsdatum sein";
-        
+        submit.disabled = true;
         return;
     }
 });
 
-var phonenumber = document.getElementById('phonenumber');
 phonenumber.addEventListener('focusout', function() {
     errorOutput = document.getElementById('wrongPhoneNumber');
     if (phonenumber.value.match(phonenumberRegEx)) 
     {
         phonenumber.style.border = "1px solid green";
         errorOutput.style.display = "none";
+        submit.disabled = false;
+        activateSumbit() 
         return;
     }
     else
@@ -82,18 +106,19 @@ phonenumber.addEventListener('focusout', function() {
         errorOutput.style.display = "block";
         errorOutput.style.color = "red";
         errorOutput.innerHTML = "Telefonnummer muss mindestens 6 Stellen haben und<br> darf nur Zahlen enthalten";
-        
+        submit.disabled = true;
         return;
     }
 });
 
-var email = document.getElementById('email');
 email.addEventListener('focusout', function() {
     errorOutput = document.getElementById('wrongEMail');
     if (email.value.match(emailRegEx)) 
     {
         email.style.border = "1px solid green";
         errorOutput.style.display = "none";
+        submit.disabled = false;
+        activateSumbit() 
         return;
     }
     else
@@ -102,18 +127,19 @@ email.addEventListener('focusout', function() {
         errorOutput.style.display = "block";
         errorOutput.style.color = "red";
         errorOutput.innerHTML = "Email ist keine valide Email";
-        
+        submit.disabled = true;
         return;
     }
 });
 
-var password = document.getElementById('password');
 password.addEventListener('focusout', function() {
     errorOutput = document.getElementById('wrongPassword');
     if (password.value.match(passwordRegEx)) 
     {
         password.style.border = "1px solid green";
         errorOutput.style.display = "none";
+        submit.disabled = false;
+        activateSumbit() 
         return;
     }
     else
@@ -122,7 +148,7 @@ password.addEventListener('focusout', function() {
         errorOutput.style.display = "block";
         errorOutput.style.color = "red";
         errorOutput.innerHTML = "Passwort muss mindestens 8 Stellen haben und<br> muss mindestens 1 Großbuchstabe, 1 Kleinbuchstabe und 1 Zahl enthalten ";
-        
+        submit.disabled = true;
         return;
     }
 });
