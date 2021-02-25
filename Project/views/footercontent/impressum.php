dd4a427a766ccff4dd26eb5c365e1e363e749c17
DanielMichel350 <73823199+DanielMichel350@users.noreply.github.com>
2021-02-25 10:32:46 +0100

    Doku und Link im Impressum

--- Project/views/footercontent/impressum.php
+++ Project/views/footercontent/impressum.php
@@ -1,3 +1,7 @@
 <div class="impressum-body">
     <h1>Impressum</h1>
+    <h3>Gruppenprojekt zur Webentwicklung</h3>
+    <p>Von: Martin Kühlborn, Felix Polte, Daniel Michel </p>
+    <p>Bildquellen: Unsere Produktbilder haben wir uns von BurgerKing geliehen (<a href="https://www.burgerking.de/">https://www.burgerking.de/</a>)</p>
+    <p><a href="index.php?c=pages&a=doku">Hier</a> geht es zur Doku.</p>
 </div>
